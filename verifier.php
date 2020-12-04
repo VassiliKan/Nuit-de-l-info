38b0227e57221b43e3cec5196e669294aeea8413
Adrien <skillblokx@gmail.com>
2020-12-04 03:36:17 +0100

    login clean

--- verifier.php
+++ verifier.php
@@ -1,19 +1,37 @@
 <?php
 
+$server ="localhost";
+$login ="debian";
+$mdp ="ehman";
+$bdd ="surf";
+$user = $_POST['user_reg'];
+
+
+try {
+    $linkpdo = new PDO("mysql:host=$server;dbname=$bdd", $login, $mdp);
+    echo "tes co";
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+echo "troll";
 if(isset($_POST['user_log']) AND isset($_POST['password_log']))
+    echo "troll";
  {
      if(preg_match('#^[a-zA-Z0-9.]{3,15}$#',$_POST['user_log']))
      {
-         $req = $bdd->prepare('SELECT * FROM user WHERE username = :username');
+         $req = $linkpdo->prepare('SELECT * FROM user WHERE username = :username');
          $req->bindParam(':username',$_POST['user_log']);
          $req->execute();
          $req = $req->fetch();
+         echo "test";
          if(isset($req['password']))
          {
-            if( password_verify($_POST['password_log'],$req['password']))
+            echo "testtt";
+            if(password_verify($_POST['password_log'], $req['password']))
             {
                 $_SESSION['user'] = $_POST['user_log'];
-                header('Location: ../../index.php');
+                header('Location: ../../index.html');
 
             }
             else die("<p>Le mot de passe est invalide <a href=\"../../page/connexion.php\"> Cliquez ici pour recommencer</a></p>");
